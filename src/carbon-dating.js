4fae6ad2fbedd5279685974cd74995a5bcf8c1b7
andrei alekseev <aimdragman2@yandex.ru>
2023-04-10 08:33:54 +0300

    feat: complete carbon-dating, hanoi-tower and mac-address tasks

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,24 +2,32 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
+const RATIO = 0.693;
 
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string' || isNaN(sampleActivity)) {
+    return false;
+  } else {
+    const sampleActivityNumber = Number(sampleActivity);
+    if (sampleActivityNumber > 15 || sampleActivityNumber <= 0) {
+      return false;
+    }
+    return Math.ceil((Math.log(MODERN_ACTIVITY / sampleActivityNumber) * HALF_LIFE_PERIOD) / RATIO);
+  }
 }
 
 module.exports = {
