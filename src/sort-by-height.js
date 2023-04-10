c9b6e61a2adb95707685a22d7807b400c62859da
andrei alekseev <aimdragman2@yandex.ru>
2023-04-10 10:58:49 +0300

    feat: complete sort-by-height, sum-digits and transform-array tasks

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,9 +11,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  const clone = [...arr];
+  const res = clone.sort((a, b) => a - b).filter((el) => el !== -1);
+  arr.forEach((el, ind) => {
+    if (el === -1) {
+      res.splice(ind, 0, -1)
+    }
+  });
+  return res;
 }
 
 module.exports = {
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,21 +1,52 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+const params = {
+  discardPrev: "--discard-prev",
+  doublePrev: "--double-prev",
+  discardNext: "--discard-next",
+  doubleNext: "--double-next"
+};
+
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error('\'arr\' parameter must be an instance of the Array!')
+  }
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case params.discardPrev:
+        i > 0 && result.pop();
+        break;
+      case params.doublePrev:
+        i > 0 && result.push(arr[i - 1]);
+        break;
+      case params.discardNext:
+        arr.splice(i, 1);
+        arr.splice(i + 1, 1);
+        break;
+      case params.doubleNext:
+        arr[i + 1] && result.push(arr[i + 1])
+        break;
+      default:
+        result.push(arr[i]);
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
